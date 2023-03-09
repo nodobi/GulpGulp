1cf4bef06e93f31738acfde1c9f2ff5d84f961c5
DohyeokKim <hyeokdokim@gmail.com>
2023-03-09 14:45:36 +0900

    Inital setting

---
+++ app/src/main/java/com/dohyeok/gulpgulp/view/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.dohyeok.gulpgulp.view.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dohyeok.gulpgulp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
